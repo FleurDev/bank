86ebd96d5638da55294ef06d4f5e24cc0d31a9c6
fleurDEV <fleur.rouge2021a@gmail.com>
2022-03-08 16:04:55 +0100

    add equals and hascode method to fix deposit operation test

--- src/main/java/com/sg/kata/bankaccount/Balance.java
+++ src/main/java/com/sg/kata/bankaccount/Balance.java
@@ -1,5 +1,7 @@
 package com.sg.kata.bankaccount;
 
+import java.util.Objects;
+
 public class Balance {
 
 	private long balanceAfterOperation;
@@ -15,5 +17,22 @@ public class Balance {
 			return new Balance(balanceAfterOperation - amount.getAmount());
 		}
 		throw new IllegalArgumentException("Operation is not supported");
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(balanceAfterOperation);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Balance other = (Balance) obj;
+		return balanceAfterOperation == other.balanceAfterOperation;
 	}
 }
