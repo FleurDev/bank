2caa6469045a8aaece94c9fe92528fcd745d0526
fleurDEV <fleur.rouge2021a@gmail.com>
2022-03-08 16:10:16 +0100

    create test to throw exception when amount to deposit is invalid

--- src/test/java/com/sg/kata/bankaccount/AccountTest.java
+++ src/test/java/com/sg/kata/bankaccount/AccountTest.java
@@ -1,7 +1,7 @@
 package com.sg.kata.bankaccount;
 
 import static org.junit.Assert.assertEquals;
-
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -24,5 +24,14 @@ public class AccountTest {
         account.deposit(new Amount(amount));
         Balance expectedBalance = new Balance(50L);
         assertEquals(expectedBalance, account.getBalance());
+    }
+	
+	@Test(expected = InvalidBankTransactionException.class)
+	public void should_throw_exception_when_invalid_bank_transaction() 
+			throws InvalidBankTransactionException 
+	{
+		long amount = -50L;
+        account.deposit(new Amount(amount));
+        Assert.fail();
     }
 }
